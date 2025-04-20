863e37822267ac4f3763269d275899660ebc1799
dscss1 <86515441+dscss1@users.noreply.github.com>
2025-04-20 11:05:42 +0200

    Updated url to app

--- src/App.tsx
+++ src/App.tsx
@@ -54,7 +54,7 @@ function App() {
                 </div>
                 <br />
 
-                <a href="tg:dscss1" className="contact-button">
+                <a href="https://t.me/dscss1" className="contact-button">
                     Связаться со мной
                 </a>
             </div>
